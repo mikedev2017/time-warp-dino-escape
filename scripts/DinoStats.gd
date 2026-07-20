6c638659e5d116d1e291141cce8ba5dec421dd8d
mikedev2017 <mwanimationwork@gmail.com>
2026-07-20 17:31:18 -0400

    Create enemy resource and enemy controller class

--- scripts/DinoStats.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name DinoStats
-
-@export var health: int = 100
-@export var attack_damage: int = 10
-@export var speed: float = 20.0
---
+++ scripts/enemies/EnemyStats.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name EnemyStats
+
+@export var max_health: int = 100
+@export var attack_damage_amount: int = 10
+@export var speed: float = 20.0
+@export var is_flying_type: bool = false
+@export var is_boss: bool = false
+@export var enemy_texture: Texture2D
